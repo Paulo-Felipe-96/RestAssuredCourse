20e032429d35257e6ea4a61b7be30acd16e276c3
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-05-27 00:59:56 -0300

    feat: hamcrast matchers

--- src/test/java/br/ce/wcaquino/rest/OlaMundoJUnitTesting.java
+++ src/test/java/br/ce/wcaquino/rest/OlaMundoJUnitTesting.java
@@ -4,10 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class OlaMundoJUnitTesting {
     @Test
@@ -27,9 +33,31 @@ public class OlaMundoJUnitTesting {
     @Test
     public void devoConhecerOutrasFormasRestAssured() {
         given()//Pré-condições
-        .when()//Ação
-            .get("http://restapi.wcaquino.me/ola")
-        .then()//Assertivas
-            .statusCode(201);
+                .when()//Ação
+                .get("http://restapi.wcaquino.me/ola")
+                .then()//Assertivas
+                .statusCode(201);
+    }
+
+    @Test
+    public void devoConhecerOsMatchersHamcrest() {
+        assertThat("Maria", Matchers.is("Maria"));
+        assertThat(128, Matchers.isA(Integer.class));
+        assertThat(128d, Matchers.isA(Double.class));
+        assertThat(128d, Matchers.greaterThan(1d));
+        assertThat(128d, Matchers.greaterThan(1d));
+        assertThat(120d, lessThan(121d));
+
+        List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9);
+
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1, 3, 5, 7, 9));
+        assertThat(impares, containsInAnyOrder(9, 1, 5, 7, 3));
+        assertThat(impares, hasItem(1));
+        assertThat(impares, hasItems(1, 5));
+
+        assertThat("Maria", not("João"));
+        assertThat("Maria", anyOf(is("Maria"), is("João")));
+        assertThat("Maria", allOf(startsWith("Ma"), containsString("ri"), endsWith("a")));
     }
 }
