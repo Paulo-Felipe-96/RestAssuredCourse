c10c6f50ea853b295e15dd2f3e19edbe8b8c2089
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-05-27 00:30:31 -0300

    fix: refactoring with fluent code and adding static method import

--- src/test/java/br/ce/wcaquino/rest/OlaMundoJUnitTesting.java
+++ src/test/java/br/ce/wcaquino/rest/OlaMundoJUnitTesting.java
@@ -7,18 +7,29 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+
 public class OlaMundoJUnitTesting {
     @Test
     public void testOlaMundo() {
-        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+        Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
         Assert.assertEquals("Ola Mundo!", response.getBody().asString());
         //Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
         //Assert.assertTrue("O status code deveria ser 201", response.statusCode() == 201);
-        Assert.assertEquals(201, response.statusCode());
+        Assert.assertEquals(200, response.statusCode());
 
         // falha: assertivas não correspondentes (expect(200) to equal(500))
         // erro: runtime, durante a execução do código
         ValidatableResponse validation = response.then();
         validation.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured() {
+        given()//Pré-condições
+        .when()//Ação
+            .get("http://restapi.wcaquino.me/ola")
+        .then()//Assertivas
+            .statusCode(201);
     }
 }
